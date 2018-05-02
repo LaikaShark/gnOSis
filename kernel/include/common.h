ef4825c793adc2803a1d01a161762737fc6b7738
ackgg3 <ackgg3@mst.edu>
2018-05-01 20:54:08 -0500

    Added function library

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -1,8 +1,16 @@
+#ifndef common_h
+#define common_h
+
 #include <system.h>
 
 #define OS_NAME "gnOSis"
 #define OS_ARCH "x86"
 #define USER	"osdev"
-
+typedef void(*func_ptr)();
 void init_desc_tables();
 void boot_text();
+void init_funclib();
+void reg_function(void (*func)());
+void hello();
+void call_function(int in);
+#endif
--- kernel/module/common.cc
+++ kernel/module/common.cc
@@ -19,6 +19,30 @@ void boot_text()
   printj("\n");
 }
 
+func_ptr func_lib[256] = {0};
+void init_funclib()
+{
+	printj("Initializing Library....");
+	reg_function(&hello);
+	printj("DONE\n");
+}
+                 
+void reg_function(void (*func)())
+{
+	int i = 0;
+	while(func_lib[i] != 0)
+	{
+		i++;
+	}
+	func_lib[i] = func;
+}
 
-                                               
+void hello()
+{
+	printj("Hello from the library\n");
+}
 
+void call_function(int in)
+{
+	func_lib[in]();
+}
