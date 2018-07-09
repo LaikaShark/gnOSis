15e26427bf25b8e0ce0af3e852ab04fd250c2f47
Aver <ackgg3@mst.edu>
2018-07-09 17:46:57 +0000

    removed function library prototypes

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -9,9 +9,9 @@
 typedef void(*func_ptr)();
 void init_desc_tables();
 void boot_text();
-void init_funclib();
-void reg_function(void (*func)());
-void hello();
-void hello2();
-void call_function(int in);
+//void init_funclib();
+//void reg_function(void (*func)());
+//void hello();
+//void hello2();
+//void call_function(int in);
 #endif
